b36363c07cc6de271fc1d025c7f1bd78a8e13caf
Andrey Markov <markovandrew@yandex.ru>
2021-12-23 13:59:09 +0700

    add home

---
+++ js/object-fit-IE.js
@@ -0,0 +1,9 @@
+if (CSS.supports('object-fit', 'cover')) {
+    const images = document.querySelectorAll('img.object-fit');
+    images.forEach(image => {
+        const parent = image.parentElement;
+        const src = image.src;
+        parent.classList.add('object-fit-parent');
+        parent.style.backgroundImage = `url(${src})`;
+    });
+}
